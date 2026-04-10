59ac4cb3d82d298659ec41dc445cd746b9432014
vaishnav <vaishnssavkunschuaa@gmail.com>
2026-04-10 21:24:09 +0530

    ArrayListOperations

--- src/ArrayListOperations.java
+++ src/ArrayListOperations.java
@@ -1,10 +1,41 @@
+
+
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
 public class ArrayListOperations {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> cart = new ArrayList<>();
+
+        // 1. Read the number of items N
+        if (sc.hasNextInt()) {
+            int n = sc.nextInt();
+
+            // 2. Add N items to the list
+            for (int i = 0; i < n; i++) {
+                cart.add(sc.next());
+            }
+
+            // 3. Read the search term
+            String searchItem = sc.next();
+
+            // 4. Sort the items alphabetically
+            Collections.sort(cart);
+
+            // 5. Print the sorted list
+            System.out.println("Sorted Items: " + cart);
+
+            // 6. Check if the search item exists and print result
+            if (cart.contains(searchItem)) {
+                System.out.println("Found");
+            } else {
+                System.out.println("Not Found");
+            }
+        }
         
+        sc.close();
     }
 }
