c3506ce04b6ae47b2c3008353a811085acbd7ca5
vaishnav <vaishnssavkunschuaa@gmail.com>
2026-04-10 21:50:41 +0530

    Rename class to Main for autograder

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,26 @@
-
-
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
-public class ArrayListOperations {
+// Change class name to Main to satisfy the autograder
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<String> cart = new ArrayList<>();
 
-        // 1. Read the number of items N
         if (sc.hasNextInt()) {
             int n = sc.nextInt();
 
-            // 2. Add N items to the list
             for (int i = 0; i < n; i++) {
                 cart.add(sc.next());
             }
 
-            // 3. Read the search term
             String searchItem = sc.next();
 
-            // 4. Sort the items alphabetically
             Collections.sort(cart);
 
-            // 5. Print the sorted list
             System.out.println("Sorted Items: " + cart);
 
-            // 6. Check if the search item exists and print result
             if (cart.contains(searchItem)) {
                 System.out.println("Found");
             } else {
